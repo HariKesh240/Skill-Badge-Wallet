d0335f330b5828820bfcd3d5498233516d91a2e4
Harikesh M <hkesh2050@gmail.com>
2026-04-21 09:11:13 +0530

    Updated project

--- backend/server.js
+++ backend/server.js
@@ -549,4 +549,6 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
